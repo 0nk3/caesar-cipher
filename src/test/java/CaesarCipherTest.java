515756bcab0bd7cba62481b77365a68827e8eae2
0nk3 <0nk3.0nk3@gmail.com>
2020-05-17 15:52:50 +0200

    code clean up

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -1,11 +1,9 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CaesarCipherTest {
 
     private final CaesarCipher cipher = new CaesarCipher();
-
     @Test
     void emptyMessageTest(){
         assertEquals("", cipher.encrypt("", 3));
